21eac7c19a18537a6054bc1d5ca764a7a71bc23d
Jayson Virissimo <jayson.virissimo@asu.edu>
2023-02-03 16:00:32 -0700

    Charge import duties

--- lib/subscriber/line_item.rb
+++ lib/subscriber/line_item.rb
@@ -2,6 +2,7 @@
 
 module Subscriber
   class LineItem
+    IMPORT_DUTY = 0.05 # 5%
     SALES_TAX = 0.10 # 10%
     attr_reader :quantity, :type, :price
 
@@ -11,8 +12,22 @@ module Subscriber
       @price = price
     end
 
+    def import_duty
+      if qualifies_for_duty?
+        (quantity * price * IMPORT_DUTY)
+      else
+        0
+      end
+    end
+
     def sales_tax
       (quantity * price * SALES_TAX).round(2)
+    end
+
+    private
+
+    def qualifies_for_duty?
+      type.include?("imported")
     end
   end
 end
--- spec/line_item_spec.rb
+++ spec/line_item_spec.rb
@@ -1,4 +1,22 @@
 RSpec.describe Subscriber::LineItem do
+  describe "import_duty" do
+    subject do
+      described_class.new(1, type, 10.00)
+    end
+
+    context "when imported" do
+      let(:type) { "imported box of chocolates" }
+
+      specify { expect(subject.import_duty).to eq(0.50) }
+    end
+
+    context "when not imported" do
+      let(:type) { "foo bars" }
+
+      specify { expect(subject.import_duty).to be_zero }
+    end
+  end
+
   describe "#sales_tax" do
     subject do
       described_class.new(1, "music CD", 14.99)

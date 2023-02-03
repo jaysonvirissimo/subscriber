a40a2f82e2fec86956f12a69f099b2f0d98bb27a
Jayson Virissimo <jayson.virissimo@asu.edu>
2023-02-03 14:52:19 -0700

    Initialize project

---
+++ lib/subscriber.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "subscriber/version"
+
+module Subscriber
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/subscriber/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Subscriber
+  VERSION = "0.1.0"
+end
